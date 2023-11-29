de0decbc45bd706f7f0f0532c0e4da90682e6512
ide-jun <IDEdevelop16@gmail.com>
2023-11-29 12:42:11 +0900

    第5回講義

---
+++ 05/learning-path.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+data_dir = "./data"
+data_dir_path = Path(data_dir).resolve()
+print("--- Displaying the absolute path ---")
+print(data_dir_path)
+
+# dataディレクトリ以下のすべてのファイルを取得
+file_list = list(data_dir_path.glob("*"))
+print("--- Displaying all files underneath ---")
+for file in file_list:
+    print(str(file))
+
+base_dir = Path("/base_dir")
+subdirectory_name = "data_dir"
+
+new_path = base_dir / subdirectory_name
+
+print("--- joint paths ---")
+print(str(new_path))
+for file in file_list:
+    if file.exists():
+        print(f"The file '{file}' exists.")
+    else:
+        print(f"The file '{file}' dpes not exist.")
+
+# 作成したいディレクトリのパス
+dir_path = Path("./target_dir")
+
+# ディレクトリを作成
+dir_path.mkdir(parents=True, exist_ok=True)
